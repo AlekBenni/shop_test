e0219a44b2b22125ec4e8e0faafad0a1793c5ce5
Alek Benny <adeyva@mail.ru>
2021-08-11 20:28:58 +0300

    make better

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'MainController@index');
+Route::get('/categories', 'MainController@categories');
+Route::get('/{category}', 'MainController@category');
+Route::get('/mobiles/{product?}', 'MainController@product');
+
+
+// iphone_x_64
